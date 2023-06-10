ddb312edfbcdd8d730446fe6e11246b1986ead69
Nick <highvampire@gmail.com>
2023-06-10 16:41:52 +0300

    Zadaca_4_ch

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4
+
+Console.Write ("Введите 1-ое целое число ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите 2-ое целое число ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите 3-ье целое число ");
+int num3 = Convert.ToInt32 (Console.ReadLine());
+int max = num1;
+if (num2 > max)
+   {
+    max = num2;
+   }
+if (num3 > max)
+   {
+    max = num3;
+   }
+Console.Write ($"Максимальное число из 3-х -> {max}");
