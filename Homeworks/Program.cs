5de4eee072052a09febb1a81959677ee3c81fb3f
Nick <highvampire@gmail.com>
2023-06-10 16:25:46 +0300

    задача 2.ver.2

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2
+
+Console.Write ("Введите 1-ое целое число для сравнения ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите 2-ое целое число для сравнения ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+if (num1 > num2)
+   {
+     Console.WriteLine ($"{num1} больше {num2}");
+   }
+    else
+   {
+       Console.WriteLine ($"{num2} больше {num1}");
+   }
+
+// Задача 4
+
