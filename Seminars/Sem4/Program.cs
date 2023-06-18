c84c928787c35f334b3be9b88cc6c9b88e708a73
Nick <highvampire@gmail.com>
2023-06-18 15:13:24 +0300

    sem 4 end

---
+++ Seminars/Sem4/Program.cs
@@ -0,0 +1,40 @@
+﻿// int FindSum (int a)
+// {int sum = 0;
+//     for (int a = 1; i <= a; i ++);
+//         {
+//             sum += i;
+//         }
+//         return sum;        
+// }
+
+// System.Console.WriteLine ("Input number: ");
+// int a = Convert.ToInt32 (Console.ReadLine());
+
+// System.Console.WriteLine ($"Sum of all number at 1 to A -> {FindSum(a)}");
+
+// int [] CreateRandomArray(int size, int min, int max)
+// {
+//     int [] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(min,max);
+//     }
+//     return array;
+// }
+// void  PrintArray (int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//      System.Console.Write (array[i] + " ");
+//     }
+// System.Console.WriteLine ();
+// }
+
+// System.Console.WriteLine ("Input array size: ");
+// int size = Convert.ToInt32 (Console.ReadLine());
+// System.Console.WriteLine ("Input min: ");
+// int min = Convert.ToInt32 (Console.ReadLine());
+// System.Console.WriteLine ("Input max: ");
+// int max = Convert.ToInt32 (Console.ReadLine());
+
+// PrintArray(CreateRandomArray(size, min, max));
