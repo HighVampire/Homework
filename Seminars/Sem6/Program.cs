6d44ce0a70424850127a8950cc83591588518bde
Nick <highvampire@gmail.com>
2023-06-25 14:12:47 +0300

    sem.6_zad1

---
+++ Seminars/Sem6/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 1. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, 
+//а первый - на последнем и т.д.)
+
+//[1 2 3 4 5] -> [5 4 3 2 1]
+//[6 7 3 6] -> [6 3 7 6]
+
+//[1 2 3 4 5] -> [5 4 3 2 1]
+//[6 7 3 6] -> [6 3 7 6]
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }
+// //12567
+// int[] ReverseArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length/2; i++)
+//     {
+//         int temp = array[i];
+//         array[i] = array[array.Length - i - 1];
+//         array[array.Length - i - 1] = temp;
+//     }
+//     return array;
+// }
+
+
+// System.Console.WriteLine("Input array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input minimal value of array element: ");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input maximal value of array element: ");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(size, minValue, maxValue);
+
+// PrintArray(myArray);
+
+// myArray = ReverseArray(myArray);
+// PrintArray(myArray);
